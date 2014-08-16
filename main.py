9f968f88cd66ad7b85f7ce7e7f3298dff3e499de
armando perico <aperico@stark.north>
2014-08-17 00:14:12 +0200

    Fixed index path

--- main.py
+++ main.py
@@ -17,10 +17,12 @@ settings.set_property('enable-default-context-menu',1)
 settings.set_property('enable-file-access-from-file-uris',1)
 settings.set_property('enable-universal-access-from-file-uris',1)
 
-
+aDir = os.path.dirname(__file__)
 web.set_settings(settings)
 
-web.open("gui/index.html")
+index = os.path.abspath(os.path.join(aDir,"gui","index.html"))
+print "openning:",index
+web.open(index)
 
 scroller.add(web)
 
