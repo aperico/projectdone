16814573c208b1f8fcf0c3209094b5b84cc7ff42
Armando Perico <pericoarmando@gmail.com>
2015-04-26 19:14:39 +0200

    basic adding and listing requirements text-mode

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,48 +1,39 @@
-import argparse
 import sys
-import sqlite3
+import logging
+import argparse
 
+from DBInterface import SQLIteDBInterface
+DBINTERFACE = None
 DBPATH = "../database/reqs.sqlite"
-
-def dbConnect():
-    return sqlite3.connect(DBPATH)
-
-def dbDisconnect(conn):
-    conn.close()
-
-def dbFetch(conn):
-    c = conn.cursor()
-
-    # Create table
-    #c.execute('''CREATE TABLE stocks
-                 #(date text, trans text, symbol text, qty real, price real)''')
-
-    # Insert a row of data
-    #c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-
-    # Save (commit) the changes
-    #conn.commit()
-
-    # We can also close the connection if we are done with it.
-    # Just be sure any changes have been committed or they will be lost.
-    for row in c.execute('SELECT * FROM requirements'):
-       print row
 
 
 
 def addRequirement(description):
-    print "NEW REQ: ", description
-    conn = dbConnect()
-    dbFetch(conn)
-    dbDisconnect(conn)
 
-    print ":)"
+    try:
+        result = DBINTERFACE.runsql("INSERT INTO requirements (description, "\
+                                "rationale, fit_criterion ) VALUES('"\
+                                +description+"', 'rat 2', 'fc 2') ", True)
+    except Exception, err:
+        logging.error(str(err))
+        return 1
+
+    return 0
+
+def listRequirements():
+    result = DBINTERFACE.fetch("SELECT * FROM requirements")
+    for r in result:
+        print r
+
 
 def main():
+
     parser = argparse.ArgumentParser(description="Requirements Management")
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--add", action="store_true", help=
                         "Add a new requirement")
+    group.add_argument("--list", action="store_true", help=
+                        "List all requirements")
     #group.add_argument("-v", "--verbose", action="store_true")
     #group.add_argument("-q", "--quiet", action="store_true")
     parser.add_argument("--description", help= "Requirement description")
@@ -54,16 +45,37 @@ def main():
             print "Please provide --description"
             ret = 1
         else:
-            addRequirement(args.description)
-            ret = 0
+            ret = addRequirement(args.description)
+            if ret == 0:
+                print "[OK] Requirement added correctly"
+            else:
+                print "[ERROR](%s) Requirement not added" % ret
+    elif args.list:
+        listRequirements()
     else:
         parser.print_help()
+
 
     return ret
 
 
 
 if __name__ == "__main__":
+    logger = logging.getLogger()
+    logger.disabled = False
+
+
+    logging.basicConfig(filename='backendlogs.log', level=logging.INFO,
+    format= "'%(asctime)s - %(module)s:%(lineno)s %(funcName)s - %(message)s'")
+
+    logging.info('--------- Started --------')
+
+
+    DBINTERFACE = SQLIteDBInterface(DBPATH)
+    DBINTERFACE.connect()
     ret = main()
+    DBINTERFACE.disconnect()
+
+    logging.info('------ Finished with '+ str(ret)+" --------\n")
     sys.exit(ret)
 
