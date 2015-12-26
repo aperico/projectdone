e4f49d44bcdeaa8dce5c67e824635f2a4d66e529
Armando Perico <pericoarmando@gmail.com>
2015-12-26 17:48:21 +0100

    saving current state

---
+++ backend/app/requirements/RequirementsManager.py
@@ -0,0 +1,17 @@
+'''
+Created on May 3, 2015
+
+@author: aperico
+'''
+
+class RequirementsManager(object):
+    '''
+    classdocs
+    '''
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+
+    def
